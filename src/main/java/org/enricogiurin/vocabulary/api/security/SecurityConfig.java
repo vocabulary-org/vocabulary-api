ef48f1c4faf0654554416d5ce5e4e6b268a7d2b9
Enrico Giurin <enricogiurin@gmail.com>
2024-11-12 02:23:49 +0100

    Defined local profile. Refactoring of security beans

--- src/main/java/org/enricogiurin/vocabulary/api/security/SecurityConfig.java
+++ src/main/java/org/enricogiurin/vocabulary/api/security/SecurityConfig.java
@@ -21,25 +21,14 @@ package org.enricogiurin.vocabulary.api.security;
  */
 
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.enricogiurin.vocabulary.api.model.User;
-import org.enricogiurin.vocabulary.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -47,8 +36,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig {
-
-  private final UserRepository userRepository;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http,
@@ -66,38 +53,5 @@ public class SecurityConfig {
         .oauth2Login(Customizer.withDefaults())
         .formLogin(Customizer.withDefaults())
         .build();
-  }
-
-  //TODO - refactor this
-  @Bean
-  GrantedAuthoritiesMapper userAuthoritiesMapper() {
-    return (authorities) -> {
-      Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-      authorities.forEach(authority -> {
-        if (OidcUserAuthority.class.isInstance(authority)) {
-          log.warn("OidcUserAuthority case not implemented");
-        } else if (OAuth2UserAuthority.class.isInstance(authority)) {
-          OAuth2UserAuthority oauth2UserAuthority = (OAuth2UserAuthority) authority;
-          Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
-          if (userAttributes.get("email") instanceof String authenticatedEmail) {
-            log.info("Authenticated user's email: {}", authenticatedEmail);
-            // Map the attributes found in userAttributes
-            // to one or more GrantedAuthority's and add it to mappedAuthorities
-            Optional<User> optionalUser = userRepository.findByEmail(authenticatedEmail);
-            if (optionalUser.isPresent()) {
-              mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + Roles.USER.name()));
-              if (optionalUser.get().isAdmin()) {
-                mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + Roles.ADMIN.name()));
-              }
-
-            } else {
-              log.info("{} is an authenticated user but not registered yet!", authenticatedEmail);
-            }
-
-          }
-        }
-      });
-      return mappedAuthorities;
-    };
   }
 }
