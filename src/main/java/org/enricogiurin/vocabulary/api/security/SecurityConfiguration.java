5276787718d29e3dcba29780dd7e6f610153db2e
Enrico Giurin <enricogiurin@gmail.com>
2025-08-10 03:43:10 +0200

    fixing cors & security issues

--- src/main/java/org/enricogiurin/vocabulary/api/security/SecurityConfiguration.java
+++ src/main/java/org/enricogiurin/vocabulary/api/security/SecurityConfiguration.java
@@ -21,60 +21,82 @@ package org.enricogiurin.vocabulary.api.security;
  */
 
 
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 class SecurityConfiguration {
 
   private final KeycloakJwtTokenConverter keycloakJwtTokenConverter;
 
   private final String allowedOrigins;
 
+  @Value("${spring.websecurity.debug:false}")
+  private boolean webSecurityDebug;
+
   SecurityConfiguration(TokenConverterProperties properties,
       @Value("${application.cors.allowed-origins}") String allowedOrigins) {
     JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
         new JwtGrantedAuthoritiesConverter();
-    this.keycloakJwtTokenConverter = new KeycloakJwtTokenConverter(
-        jwtGrantedAuthoritiesConverter, properties);
+    this.keycloakJwtTokenConverter =
+        new KeycloakJwtTokenConverter(jwtGrantedAuthoritiesConverter, properties);
     this.allowedOrigins = allowedOrigins;
   }
+
+  @Bean
+  WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.debug(webSecurityDebug);
+  }
+
 
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http)
       throws Exception {
-    return http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-            .requestMatchers("/actuator/**").permitAll()
-            .requestMatchers("/api/**").authenticated()
-            .anyRequest().permitAll())
-        .oauth2ResourceServer(oauth2 -> oauth2
-            .jwt(jwt -> jwt.jwtAuthenticationConverter(
-                keycloakJwtTokenConverter)))
-        .sessionManagement(session -> session.sessionCreationPolicy(
-            SessionCreationPolicy.IF_REQUIRED))
+    return http.csrf(csrf -> csrf.disable()).cors(Customizer.withDefaults())
+        .authorizeHttpRequests(
+            authorizeRequests -> authorizeRequests
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll())
+        .oauth2ResourceServer(
+            oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakJwtTokenConverter)))
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
         .build();
   }
 
+
   @Bean
-  WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(allowedOrigins.split(","))
-            .allowedMethods("GET", "POST", "PUT", "PATCH",
-                "DELETE");
-      }
-    };
+  CorsConfigurationSource corsConfigurationSource() {
+    log.info("Configuring CORS with allowed origins: {}", allowedOrigins);
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
+
+
+
+
 }
